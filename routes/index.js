f59911924c3efa58c6d9e4bec3eeae7f5f4ab00d
Jong Hoon Lee <whdgns0665@Jongs-Air.fios-router.home>
2019-02-12 23:43:49 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var authHelper = require('../helpers/auth');
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  let parms = { title: 'Home', active: { home: true } };
+
+  const accessToken = await authHelper.getAccessToken(req.cookies, res);
+  const userName = req.cookies.graph_user_name;
+  const userEmail = req.cookies.user_email;
+
+  if (accessToken && userName) {
+    parms.user = userName;
+    parms.email = userEmail;
+    //parms.debug = `User: ${userName}\nAccess Token: ${accessToken}`;
+  } else {
+    parms.signInUrl = authHelper.getAuthUrl();
+    parms.debug = parms.signInUrl;
+  }
+
+  res.render('index', parms);
+});
+
+module.exports = router;
